c62ab6310281984534d12ec4adbc2c87af6b6a1f
chassape <Perrine.Chassat@grenoble-inp.org>
2019-01-10 10:50:28 +0100

    Suppression des using namespace

--- src/test_image.cpp
+++ src/test_image.cpp
@@ -2,18 +2,17 @@
 #include <opencv2/opencv.hpp>
 #include "test_image.hpp"
 
-using namespace std;
 
 void test_image(char const *arg[]){
-  string m_name = (string)arg[1];
-  Mat m_image;
-  m_image = imread(m_name, CV_LOAD_IMAGE_GRAYSCALE);
+  std::string m_name = (std::string)arg[1];
+  cv::Mat m_image;
+  m_image = cv::imread(m_name, CV_LOAD_IMAGE_GRAYSCALE);
   Image im1(m_image, m_name);
   //im1.display_attributes();
   im1.display_Mat();
   // im1.save_Mat();
   // im1.save_Mat("other_name.png");
-  // cout << im1.min_intensity() << im1.max_intensity();
+  // std::cout << im1.min_intensity() << im1.max_intensity() << std::endl;
   // unsigned int origine[2] = {30,0};
   // im1.draw_rectangle(1,origine,30,300);
   // im1.draw_rectangle(0,origine,10,10);
