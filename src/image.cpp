0b268d17b7442a4a5b7570334cd5fef8da3b2939
SpaceLauncher <lucas.etourneau1@outlook.fr>
2019-01-14 18:18:01 +0100

    Declare includes in good files

--- src/image.cpp
+++ src/image.cpp
@@ -5,12 +5,6 @@
  * \date 01/08/2019
  */
 
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/types_c.h>
-#include <vector>
-#include <string.h>
-
 #include "image.hpp"
 
 /*!
