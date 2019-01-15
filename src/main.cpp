7a7d46827cbb1dcd02e624a6a4a7f604b27bfefd
gauffra <gauffrea@grenoble-inp.org>
2019-01-15 18:01:16 +0100

    Fix Cmake minor compatibility bug

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,15 @@ using namespace std;
 #include "tests.hpp"
 
 int main(int argc, char const *argv[]) {
-  cout << "Test image :" << endl;
-  test_image(argv);
-  cout << "Test pressure :" << endl;
-  test_pressure(argv);
-  cout << "Test rotation :" << endl;
-  test_rotation(argv, M_PI/4, Pixel(128, 128));
-  cout << "Test optimization :" << endl;
-  test_optimization(argv);
+  // cout << "Test image :" << endl;
+  // test_image(argv);
+
+  // cout << "Test pressure :" << endl;
+  // test_pressure(argv);
+
+  // cout << "Test rotation :" << endl;
+  // test_rotation(argv, M_PI/4, Pixel(128, 128));
+
   cout << "Test warp :" << endl;
   test_warp(argv, 10, Pixel(128, 128));
   return 0;
--- src/tests.cpp
+++ src/tests.cpp
@@ -48,11 +48,11 @@ void test_rotation(char const *arg[], float angle, Pixel rot_point) {
 void test_optimization(char const *arg[]){
   std::string m_name = (std::string)arg[1];
   cv::Mat m_image;
-  m_image = cv::imread(m_name, CV_LOAD_IMAGE_GRAYSCALE);
+  m_image = cv::imread(m_name, cv::IMREAD_GRAYSCALE);
   Image im1(m_image, m_name);
   std::string m_name2 = (std::string)arg[2];
   cv::Mat m_image2;
-  m_image2 = cv::imread(m_name2, CV_LOAD_IMAGE_GRAYSCALE);
+  m_image2 = cv::imread(m_name2, cv::IMREAD_GRAYSCALE);
   Image im2(m_image2, m_name2);
   int px = im1.optimization(im2);
   std::cout << px;
