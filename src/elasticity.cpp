f1bd3ff5433c873a18cc83b99cf7fe2187bfcefe
SpaceLauncher <lucas.etourneau1@outlook.fr>
2019-01-15 12:57:37 +0100

    Add elasticity file

---
+++ src/elasticity.cpp
@@ -0,0 +1,9 @@
+/*!
+ * \file elasticity.cpp
+ * \brief Definition of methods concerning the elasticity phenomenon of skin
+ * \author Perrine, Célestine, Aurélien, Lucas
+ * \date 14/08/2019
+ */
+
+#include "image.hpp"
+
