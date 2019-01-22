f5b98fbf2bb8b833b7d36f666bb562a7e4b9e793
SpaceLauncher <lucas.etourneau1@outlook.fr>
2019-01-22 16:01:04 +0100

    Add kernel expansion function

--- src/linear_filter.cpp
+++ src/linear_filter.cpp
@@ -1,5 +1,6 @@
 #include "linear_filter.hpp"
-#include "maths_tools.hpp"
+
+
 void Image::convolute(std::vector<float> kernel){
   int a = ((int)(std::pow(kernel.size(),0.5))-1)/2;
   int b = a ;
@@ -21,4 +22,23 @@ void Image::convolute(std::vector<float> kernel){
     }
   }
   m_intensity_array = new_intensity ;
+}
+
+Image kernel_expansion(std::vector<float> kernel, int width, int height) {
+  cv::Mat extended_kernel(height, width, 0);
+  Image im(extended_kernel, "kernel_extended");
+  int a = ((int)(std::pow(kernel.size(),0.5))-1)/2;
+  int x_middle = width/2-1;
+  int y_middle = height/2-1;
+  int x_top_left = x_middle-a;
+  int y_top_left = y_middle-a;
+  int kernel_index = 0;
+  for (int i = x_top_left; i< x_top_left+width; i++) {
+    for (int j = y_top_left; j < y_top_left+height; j++) {
+      *im.get_pointer(im.coord_to_index(i,j)) = kernel[kernel_index];
+      kernel_index++;
+    }
+  }
+  im.back_to_Mat();
+  return im;
 }
