16900088958d083770d598d63bcddc32c68e0389
gauffra <gauffrea@grenoble-inp.org>
2019-01-24 15:30:33 +0100

    Fix bug in the interpolation on index which could go one value too far

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,19 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
   (void)argc;
-  cout << "*** Tests ***" << endl;
-  // cout << "Test image :" << endl;
+  // cout << "*** Tests ***" << endl;
+  // cout << "Test display original image :" << endl;
   // test_image(argv);
-
+  //
   // cout << "Test pressure :" << endl;
   // test_pressure(argv);
-
+  //
   // cout << "Test rotation :" << endl;
   // test_rotation(argv, M_PI/4, Pixel(128, 144));
   //
   // cout << "Test warp :" << endl;
   // test_warp(argv, M_PI/6, Pixel(110, 220), 50, 4);
-
+  //
 
   // cout << "Test optimization :" << endl;
   // test_optimization(argv);
@@ -30,7 +30,7 @@ int main(int argc, char const *argv[]) {
   //std::vector<float> kernel = {1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0}  ;
   std::vector<float> kernel = {-1,-1,-1,-1,9,-1,-1,-1,-1,-1}  ;
   // std::vector<float> kernel = {-1,0,1,-2,0,2,-1,0,1}  ;
-  // cout << "Test convolute" << endl ;
+  cout << "Test convolution :" << endl ;
   test_convolute_classic(argv,kernel);
 
   return 0;
