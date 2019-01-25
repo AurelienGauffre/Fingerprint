b3aaaa9fe2b96ab162d8caa4f195acc88650281a
Celestine Angla <anglac@f216-01.e-im2ag.ujf-grenoble.fr>
2019-01-25 19:41:48 +0100

    Merge

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
@@ -30,21 +32,8 @@ int main(int argc, char const *argv[]) {
   //std::vector<float> kernel = {1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0}  ;
   // std::vector<float> kernel = {-1,-1,-1,-1,9,-1,-1,-1,-1,-1}  ;
   // std::vector<float> kernel = {-1,0,1,-2,0,2,-1,0,1}  ;
-  // cout << "Test convolute" << endl ;
-  // test_convolute_classic(argv,kernel);
-  // cout << "Test optimization :" << endl;
-  // test_optimization(argv);
-
-  //std::vector<float> kernel = {0,0,0,0,1,0,0,0,0};
-  //std::vector<float> kernel = {1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0,1/9.0}  ;
-  // std::vector<float> kernel = {1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9}  ;
-  // cout << "Test convolute" << endl ;
-  // test_convolute_dft(argv,kernel);
-  // test_inv_ft(argv);
-  // std::vector<float> kernel = {-1,-1,-1,-1,9,-1,-1,-1,-1,-1}  ;
-  // std::vector<float> kernel = {-1,0,1,-2,0,2,-1,0,1}  ;
   // cout << "Test convolution :" << endl ;
-  // test_convolute_classic(argv,kernel);
+  // test_inv_ft(argv);
 
 
   return 0;
