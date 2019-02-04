da193cb36bea3ee29f5734016828c10206652a69
Celestine Angla <anglac@f214-04.e-im2ag.ujf-grenoble.fr>
2019-02-04 12:06:22 +0100

    Refine opti algo rxy

--- demo/image_demo.cpp
+++ demo/image_demo.cpp
@@ -6,26 +6,14 @@ int main(int argc, char const *argv[]) {
   cv::Mat m_image;
   m_image = cv::imread(m_name, cv::IMREAD_GRAYSCALE);
   Image im1(m_image, m_name);
-  // im1.display_attributes();
-  // im1.display_Mat();
-  // im1.save_Mat();
-  // im1.save_Mat("other_name.png");
-  // std::cout << im1.min_intensity() << im1.max_intensity() << std::endl;
-  // int origine[2] = {30,0};
-  // im1.draw_rectangle(1,origine,30,300);
-  // im1.draw_rectangle(0,origine,10,10);
-  // im1.display_Mat();
-  // im1.symetry_y();
-  // im1.display_Mat();
-  // im1.symetry_diag();
-  //
 
   Image im2(im1);
   int origine[2] = {30,0};
   im1.draw_rectangle(1,origine,30,300);
+  im2.symetry_x();
   im2.display_Mat();
   im1.display_Mat();
-  // im1.data_intensity();
+
   return 0;
 
 }
